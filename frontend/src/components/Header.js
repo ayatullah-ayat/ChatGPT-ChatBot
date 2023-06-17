478948511a701e8dc71acb4f519d6c253c836669
Ayatullah <56090060+ayatullah-ayat@users.noreply.github.com>
2023-06-18 01:10:34 +0600

    Login, Register page

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,24 +2,32 @@
 import { Link } from "react-router-dom";
 
 const Header = () => {
+
+
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
-                <Link class="navbar-brand" to="/">ChatBot</Link>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <Link className="navbar-brand" to="/">ChatBot</Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <Link class="nav-link" to="/">Home</Link>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <Link className="nav-link" to="/">Home</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/chatbot">Chatbot</Link>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/chatbot">Chatbot</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="about">About</Link>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">About</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/register">Register</Link>
                         </li>
                     </ul>
                 </div>
