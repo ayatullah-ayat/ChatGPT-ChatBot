c19ec80aac81fd07eebeaca5fd6cd5e3c68bdfd6
Ayatullah <56090060+ayatullah-ayat@users.noreply.github.com>
2023-06-18 01:21:29 +0600

    Login form designed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ChatBot from "./pages/Chatbot";
 import About from "./pages/About";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
             <Route path="/" element={ <Home /> }/>
             <Route path="/chatbot" element={ <ChatBot /> }/>
             <Route path="/about" element={ <About /> }/>
+            <Route path="/login" element={ <Login /> }/>
         </Routes>
     </div>
   );
