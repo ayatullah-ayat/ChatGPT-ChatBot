ea086cb1121f0d7cc7cf04ae2bee21efccf03229
Ayatullah <56090060+ayatullah-ayat@users.noreply.github.com>
2023-06-18 13:22:38 +0600

    dotenv initiated

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
-const express = require('express')
-
+require('dotenv').config();
+const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.end('hello from express')
+    res.end('hello from express');
 })
 
-const PORT = 3001;
+
+
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log('server running on PORT', PORT);
