86475f6099ab4ef7793d8c6130df473fa461c2eb
Ayatullah <56090060+ayatullah-ayat@users.noreply.github.com>
2023-06-21 09:45:02 +0600

    Label component completed

---
+++ frontend/src/components/Label.js
@@ -0,0 +1,9 @@
+
+const Label = ({ className, name }) => {
+
+    return (
+        <label className={className} for="form2Example31"> { name } </label>
+    )
+}
+
+export default Label;
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Input from "../components/Input";
 import Button from "../components/Button";
+import Label from "../components/Label";
 
 const Login = () => {
 
@@ -14,19 +15,19 @@ const Login = () => {
                 <form>
                     <div className="form-outline mb-4">
                         <Input type="email" inputChangeHandler={ (e) => setEmail(e.target.value) }/>
-                        <label class="form-label" for="form2Example1">Email address</label>
+                        <Label className="form-label" name="Email Address"/>
                     </div>
 
                     <div class="form-outline mb-4">
                         <Input type="password" inputChangeHandler={ (e) => setPassword(e.target.value) }/>
-                        <label class="form-label" for="form2Example2">Password</label>
+                        <Label className="form-label" name="Password"/>
                     </div>
 
                     <div class="row mb-4">
                         <div class="col d-flex justify-content-center">
                             <div class="form-check">
                                 <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
-                                <label class="form-check-label" for="form2Example31"> Remember me </label>
+                                <Label className="form-check-label" name="Remember Me"/>
                             </div>
                         </div>
 
