5c0b0a302a792d1f8f172a5a59a70957b5319b53
Ayatullah <56090060+ayatullah-ayat@users.noreply.github.com>
2023-06-27 01:03:19 +0600

    feat: Redirect logged in User

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,27 @@
 import Header from "./components/Header";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import ChatBot from "./pages/Chatbot";
 import About from "./pages/About";
 import Login from "./pages/Login";
+import { AuthContext } from "./context";
+import { useContext } from "react";
+
 
 function App() {
-  return (
-    <div>
-        <Header />
-        <Routes>
-            <Route path="/" element={ <Home /> }/>
-            <Route path="/chatbot" element={ <ChatBot /> }/>
-            <Route path="/about" element={ <About /> }/>
-            <Route path="/login" element={ <Login /> }/>
-        </Routes>
-    </div>
-  );
+
+    const user = useContext(AuthContext);
+    return (
+        <div>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/chatbot" element={<ChatBot />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/login" element={ user ? <Login /> : <Navigate replace to={'/'} />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
